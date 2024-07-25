b773a3f2af5465c182a08c5dd591c6898ef326ba
Thierry <thierrysantoos123@gmail.com>
2024-07-25 20:33:06 -0300

    feat: init spacial representation of text

--- src/index.ts
+++ src/index.ts
@@ -23,15 +23,13 @@ function processJSON(document: ProcessDocumentResponse): string {
     return "No pages found in the document.";
   }
 
-  // TODO: handle N pages
   const page = document.pages[0];
   const { width, height, unit } = page.dimension || { width: 0, height: 0 };
 
   console.log(`width: ${width}, height: ${height} ${unit}\n`);
 
-
-  // the blocks / paragraphs / lines are not ordered, and we could order it using the bounding box position
-  const sortedBlocks = page.blocks?.slice().sort((a, b) => {
+  // Sort blocks by bounding box positions
+  const sortedBlocks = page.tokens?.slice().sort((a, b) => {
     const aY = a.layout?.boundingPoly?.normalizedVertices?.[0]?.y || 0;
     const bY = b.layout?.boundingPoly?.normalizedVertices?.[0]?.y || 0;
     const aX = a.layout?.boundingPoly?.normalizedVertices?.[0]?.x || 0;
@@ -41,11 +39,49 @@ function processJSON(document: ProcessDocumentResponse): string {
       return aX - bX;
     }
     return aY - bY;
-  });
+  }) as protos.google.cloud.documentai.v1.Document.Page.IBlock[];
 
   let text = "";
 
-  return text;
+  function concatenateText(layout) {
+    return layout.textAnchor.textSegments.map(segment => document.text.substring(segment.startIndex, segment.endIndex).trim()).join('');
+  }
+
+  const lines = [];
+  let currentLine: string[] = [];
+
+  sortedBlocks.forEach((block, index) => {
+    console.log(`Block: ${index}`)
+    console.log(`Text: ${concatenateText(block.layout)}`)
+    console.log(`Coords: y=${block.layout?.boundingPoly?.normalizedVertices?.[0].y},x=${block.layout?.boundingPoly?.normalizedVertices?.[0].x}`)
+    console.log(`-----\n`)
+
+    const blockY = block.layout?.boundingPoly?.normalizedVertices?.[0]?.y || 0;
+    const nextBlockY = sortedBlocks[index + 1]?.layout?.boundingPoly?.normalizedVertices?.[0]?.y || blockY;
+
+    currentLine.push(block);
+    // TODO: calculate it dinamically
+    if (Math.abs(blockY - nextBlockY) > 0.015) {
+      lines.push(currentLine);
+      currentLine = [];
+    }
+  });
+
+  if (currentLine.length > 0) {
+    lines.push(currentLine);
+  }
+
+  // Concatenate text from grouped lines
+  lines.forEach(line => {
+    line.forEach(block => {
+      if (block.layout) {
+        text += concatenateText(block.layout) + " ";
+      }
+    });
+    text = text.trim() + "\n";
+  });
+
+  return text.trim();
 }
 
 async function main() {
